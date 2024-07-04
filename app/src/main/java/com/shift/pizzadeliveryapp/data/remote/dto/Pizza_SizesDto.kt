4a3b7d3e5da135586abb516eba57225a6ebfec73
Miron <popovmiron@gmail.com>
2024-07-04 20:47:43 +0700

    Попытка в реализацию перевода размеров и всего остального на уровне domain

--- app/src/main/java/com/shift/pizzadeliveryapp/data/remote/dto/Pizza_SizesDto.kt
+++ app/src/main/java/com/shift/pizzadeliveryapp/data/remote/dto/Pizza_SizesDto.kt
@@ -1,7 +1,10 @@
 package com.shift.pizzadeliveryapp.data.remote.dto
 
 
+
+import androidx.core.content.ContextCompat.getString
 import com.google.gson.annotations.SerializedName
+import com.shift.pizzadeliveryapp.R
 import com.shift.pizzadeliveryapp.domain.models.Pizza_Size
 
 data class Pizza_SizesDto(
@@ -10,6 +13,16 @@ data class Pizza_SizesDto(
     @SerializedName("price")
     val price: Int
 )
+
+// я чета туплю и не понимаю как можно все переводить
+// исходя из языка приложения
+/*fun Pizza_SizesToRu(name: String) =
+    when (name) {
+        "SMALL" -> R.string.pizza_small
+        "MEDIUM" -> R.string.pizza_medium.toString()
+        "LARGE" -> R.string.pizza_large.toString()
+        else -> "Другой"
+    }*/
 
 fun Pizza_SizesDto.toPizza_Size() : Pizza_Size{
     return Pizza_Size(
