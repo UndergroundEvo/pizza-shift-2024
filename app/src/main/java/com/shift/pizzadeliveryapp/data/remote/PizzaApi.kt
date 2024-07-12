54c95baacfc315d4cf06fccf9b48ab0a04a29169
Miron <popovmiron@gmail.com>
2024-07-12 20:19:32 +0700

    Запросы signin и postpayment в api

--- app/src/main/java/com/shift/pizzadeliveryapp/data/remote/PizzaApi.kt
+++ app/src/main/java/com/shift/pizzadeliveryapp/data/remote/PizzaApi.kt
@@ -3,6 +3,11 @@ package com.shift.pizzadeliveryapp.data.remote
 import com.shift.pizzadeliveryapp.data.remote.dto.GetCatalogDto
 import com.shift.pizzadeliveryapp.data.remote.dto.OtpPhoneDto
 import com.shift.pizzadeliveryapp.data.remote.dto.OtpReturnDto
+import com.shift.pizzadeliveryapp.data.remote.dto.PaymentDto
+import com.shift.pizzadeliveryapp.data.remote.dto.PaymentStatusDto
+import com.shift.pizzadeliveryapp.data.remote.dto.SignInInputDto
+import com.shift.pizzadeliveryapp.data.remote.dto.SignInOutputDto
+import com.shift.pizzadeliveryapp.domain.models.Order
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -15,9 +20,12 @@ interface PizzaApi {
     @POST("auth/otp")
     suspend fun getOtpCode(@Body phone : OtpPhoneDto) : OtpReturnDto
 
-/*    @POST("pizza/payment")
-    suspend fun postPayment() //тут еще что-то будет
+    @POST("pizza/payment")
+    suspend fun postPayment(@Body payment: PaymentDto) : PaymentStatusDto
 
+    @POST("users/signin")
+    suspend fun signIn(@Body signin: SignInInputDto) : SignInOutputDto
+    /*
     @GET("pizza/orders")
     suspend fun getOrders()
 
