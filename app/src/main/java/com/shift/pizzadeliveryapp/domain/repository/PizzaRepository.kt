308c72a5aaedb6b7299c5917de98767b2103d233
Miron <popovmiron@gmail.com>
2024-07-12 20:21:31 +0700

    Метод signIn в репозитории

--- app/src/main/java/com/shift/pizzadeliveryapp/domain/repository/PizzaRepository.kt
+++ app/src/main/java/com/shift/pizzadeliveryapp/domain/repository/PizzaRepository.kt
@@ -2,9 +2,13 @@ package com.shift.pizzadeliveryapp.domain.repository
 
 import com.shift.pizzadeliveryapp.data.remote.dto.GetCatalogDto
 import com.shift.pizzadeliveryapp.data.remote.dto.OtpReturnDto
+import com.shift.pizzadeliveryapp.data.remote.dto.SignInInputDto
+import com.shift.pizzadeliveryapp.data.remote.dto.SignInOutputDto
+import retrofit2.http.Body
 
 interface PizzaRepository {
 
     suspend fun getPizzas() : GetCatalogDto
     suspend fun getOtpCode(phone : String) : OtpReturnDto
+    suspend fun signIn(signin: SignInInputDto) : SignInOutputDto
 }
