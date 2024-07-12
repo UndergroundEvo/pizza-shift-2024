bf26d13c4fbc2b7610e6b5ea00c0058b6f7f2a90
Miron <popovmiron@gmail.com>
2024-07-12 20:26:16 +0700

    Светлая тема с оранжевым акцентом

--- app/src/main/java/com/shift/pizzadeliveryapp/presentation/theme/Theme.kt
+++ app/src/main/java/com/shift/pizzadeliveryapp/presentation/theme/Theme.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -17,19 +18,24 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color(0xFFF4511E),
+    secondary = Color(0xFFF4511E),
+    tertiary = Color(0xFFF4511E),
+    //Other default colors to override
 
-    /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
+
+    onPrimary = Color.White, //текст на кнопках
     onSecondary = Color.White,
     onTertiary = Color.White,
+
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
-    */
+
+    secondaryContainer = Color.White,
+    onSecondaryContainer = Color(0xFFF4511E)
+
 )
 
 @Composable
@@ -40,10 +46,10 @@ fun PizzaDeliveryAppTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        /*dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+        }*/
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
