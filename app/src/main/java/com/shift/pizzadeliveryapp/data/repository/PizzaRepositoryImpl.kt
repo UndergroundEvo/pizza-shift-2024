ca8189335879d01c10d84bc83f11679aeefad9d7
Miron <popovmiron@gmail.com>
2024-07-12 20:20:30 +0700

    Метод в signin в implementation

--- app/src/main/java/com/shift/pizzadeliveryapp/data/repository/PizzaRepositoryImpl.kt
+++ app/src/main/java/com/shift/pizzadeliveryapp/data/repository/PizzaRepositoryImpl.kt
@@ -4,7 +4,10 @@ import com.shift.pizzadeliveryapp.data.remote.PizzaApi
 import com.shift.pizzadeliveryapp.data.remote.dto.GetCatalogDto
 import com.shift.pizzadeliveryapp.data.remote.dto.OtpPhoneDto
 import com.shift.pizzadeliveryapp.data.remote.dto.OtpReturnDto
+import com.shift.pizzadeliveryapp.data.remote.dto.SignInInputDto
+import com.shift.pizzadeliveryapp.data.remote.dto.SignInOutputDto
 import com.shift.pizzadeliveryapp.domain.repository.PizzaRepository
+import retrofit2.http.Body
 import javax.inject.Inject
 
 class PizzaRepositoryImpl @Inject constructor(
@@ -17,5 +20,9 @@ class PizzaRepositoryImpl @Inject constructor(
 
     override suspend fun getOtpCode(phone: String): OtpReturnDto {
         return api.getOtpCode(OtpPhoneDto(phone))
+    }
+
+    override suspend fun signIn(signin: SignInInputDto) : SignInOutputDto {
+        return api.signIn(signin)
     }
 }
